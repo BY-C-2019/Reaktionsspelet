6e1ac7b69e86bd4a84a8ebc6af7657bd6d8189ab
lyret <viktor@lyresten.se>
2019-09-30 15:30:22 +0200

    solves it

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Reaktionsspelet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game myGame = new Game();
+			myGame.StartGame();
+			Console.WriteLine("Spelet tog slut, resultatet var " + myGame.Results);
+        }
+    }
+}
